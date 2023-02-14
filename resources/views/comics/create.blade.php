e7d8b55d9dd7511e37822a443d30b06a6d1b3bd1
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-02-14 16:39:04 +0100

    ulteriore implementazione form e fix method

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,26 +1,31 @@
 @extends('layouts.app')
 @section('main-content')
 <div class="container">
-    <form action="{{route('comics.store')}}" methods="POST">
+    <form action="{{route('comics.store')}}" method="POST">
         @csrf
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="text" class="form-control" id="" >
+        <label for="exampleInputEmail1" class="form-label">titolo</label>
+        <input name="title" type="text" class="form-control" id="">
       </div>
 
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control">
+        <label for="exampleInputEmail1" class="form-label">thumb</label>
+        <input type="text" class="form-control" id="" name="thumb">
       </div>
 
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control">
+        <label for="exampleInputEmail1" class="form-label">Prezzo</label>
+        <input type="text" class="form-control" id="" name="price" >
       </div>
 
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control">
+        <label for="exampleInputEmail1" class="form-label">Serie</label>
+        <input type="text" class="form-control" id="" name="series" >
+      </div>
+
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">tipo</label>
+        <input type="text" class="form-control" id="" name="type" >
       </div>
 
       <button type="submit" class="btn btn-primary">Submit</button>
